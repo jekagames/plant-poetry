fcceebb284c17ef9eaed81ea68852d906cb95c85
jekagames <jess.ro.marcotte@gmail.com>
2017-04-05 15:20:04 -0400

    restructuring grammar

--- madlibs.js
+++ madlibs.js
@@ -21,22 +21,17 @@ var madlibs =
 	//useful commands/tags to keep in mind
 	//"#poetryLine.capitalize#.""
 	//#person.a#
-	//if you need to add a character such as "", add a backslash \ to have it read as that character and not as code
+	//if you need to add a character such as "", add a backslash  to have it read as that character and not as code
 	//Nesting New grammars: make sure they are a defined grammar, but when defining them, they follow the same
 	//syntax as the poetry line example below. When referred to in another string, add ## around them
 	//to define a grammar, see example of poetryLine below
-	// "poetryLine":
-	// [
-	// 	"#ritualCommand.capitalize#.",
-	// 	"#clearing.capitalize# your #ineffableAspect# of #unwantedThoughts#, #ritualCommand#.",
-	// 	"With #adjective.a# #bodyPart#, #ritualCommand#.",
-	// 	"With #positiveAttribute#, #ritualCommand#.",
-	// 	"With the #positiveAttribute# of #person.a#, #ritualCommand#."
 
 	"poetryLine":
 	[
 		"Planty plant #plant#",
 		"#roots# your #plant# of #plantExperience#"
+		"Planty plant #plant# #roots#",
+		"#roots# your #plant# of #plant# #roots#"
 	],
 	"plant":
 	[
